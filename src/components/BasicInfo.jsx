408802d68023cc21a0973f9067b53dc57f13c076
BenjaminJMcD <BenjaminJMcD@outlook.com>
2024-07-31 14:14:25 -0500

    restructure: basic info individual form and separate component

--- src/components/BasicInfo.jsx
+++ src/components/BasicInfo.jsx
@@ -4,35 +4,76 @@ import { useState } from "react"
 
 function BasicInfo() {
 
-    const [firstName, setFirstName] = useState("");
-    const [lastName, setLastName] = useState("");
-    const [email, setEmail] = useState("");
-    const [phone, setPhone] = useState("");
+    const [formData, setFormData]=useState({
+        firstName: "",
+        lastName: "",
+        email: "",
+        phone: "",
+    })
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        console.log("Form Submitted", formData);
+    }
+
+    const handleChange = (event) => {
+        setFormData({...formData, [event.target.name]: event.target.value})
+    }
+
+    const handleInput = (event) => {
+        const formattedPhoneNumber = formatPhoneNumber(event.target.value);
+        setFormData({...formData, [event.target.name]: formattedPhoneNumber})
+    }
+
+    function formatPhoneNumber(value) {
+        if (!value) return value;
+        const phoneNumber = value.replace(/[^\d]/g,"");
+        const phoneNumberLength = phoneNumber.length;
+
+        if (phoneNumberLength < 4) {
+            return phoneNumber
+        }
+        if (phoneNumberLength < 7) {
+            return `(${phoneNumber.slice(0, 3)}) ${phoneNumber.slice(3)}`
+        }
+        return `(${phoneNumber.slice(0,3)}) ${phoneNumber.slice(3,6)}-${phoneNumber.slice(6,10)}`
+    }
 
     return (
-        <div className="basicInfo">
+        <form onSubmit={handleSubmit}>
             <label htmlFor="firstName">First Name: </label>
             <input type="text"
                 className="firstName"
-                value = {firstName}
-                onChange={(event) => setFirstName(event.target.value)} />
+                name = "firstName"
+                required
+                value = {formData.firstName}
+                onChange={handleChange} />
             <label htmlFor="lastName">Last Name: </label>
             <input type="text" 
                 className="lastName"
-                value = {lastName}
-                onChange={(event) => setLastName(event.target.value)} />
+                name="lastName"
+                required
+                value = {formData.lastName}
+                onChange={handleChange} />
             <label htmlFor="email">Email: </label>
             <input type="email" 
                 className="email" 
-                value = {email}
-                onChange = {(event) => setEmail(event.target.value)} />
-            <label htmlFor="phoneNumber">Phone: </label>
-            <input type="phone" 
+                name="email"
+                required
+                value = {formData.email}
+                onChange = {handleChange} />
+            <label htmlFor="phone">Phone: </label>
+            <input type="tel"
+                placeholder="(123) 456 7890" 
+                pattern="\([0-9]{3}\) [0-9]{3}-[0-9]{4}"
                 className="phoneNumber"
-                value = {phone}
-                onChange={(event) => setPhone(event.target.value)} />
-        </div>
-
+                required
+                name="phone"
+                value = {formData.phone}
+                onChange={handleInput}
+                 />
+            <button className="submitButton" type="submit">SUBMIT</button>
+        </form>
     )
 
 }
