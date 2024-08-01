f1d94f9796a92e8e21a1ee88afa4e92edb5c3e50
BenjaminJMcD <BenjaminJMcD@outlook.com>
2024-08-01 13:38:17 -0500

    build: education component school, degree title, and graduated checkbox

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -3,6 +3,52 @@ import { useState } from "react";
 
 function Education() {
 
+    const [formData, setFormData]=useState({
+        school: "",
+        degreeTitle: "",
+        degree: false,
+        startDate: "mm/dd/yyyy",
+        endDate: "mm/dd/yyyy"
+    })
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        console.log("Form Submitted", formData);
+    }
+
+    const handleChange = (event) => {
+        setFormData({...formData, [event.target.name]: event.target.value})
+    }
+
+    
+    const handleClick = (event) => {
+        setFormData({...formData, [event.target.name]: event.target.checked})
+    }
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <label htmlFor="school">School: </label>
+            <input type="text"
+                className="school"
+                name = "school"
+                required
+                value = {formData.school}
+                onChange={handleChange} />
+            <label htmlFor="degreeTitle">Study: </label>
+            <input type="text" 
+                className="degreeTitle"
+                name="degreeTitle"
+                value = {formData.degreeTitle}
+                onChange={handleChange} />
+            <label htmlFor="degree">Degree Obtained: </label>
+            <input type="checkbox" 
+                className="degree" 
+                name="degree"
+                onClick = {handleClick} />
+            <button className="submitButton" type="submit">SUBMIT</button>
+        </form>
+    )
+
 }
 
 export default Education
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -5,9 +5,21 @@ import Education from "./Education";
 import Experience from "./Experience";
 
 function Form() {
-
-    <BasicInfo />
-
+    return (
+    <div>
+        <h1 className="basicInfo">General Information</h1>
+        <BasicInfo />
+        <h1 className="education">Education</h1>
+        <Education />
+        <h1 className="experience">Work Experience</h1>
+        {/* <Experience /> */}
+    </div>
+    )
 }
 
 export default Form
+
+
+// ADD MULTIPLE EDUCATION AND WORK EXPERIENCE ???
+
+// HOW TO EDIT DOCUMENT ON FORM PAGE WITH INFORMATION FROM CHILDREN +++ HOW TO CREATE DOCUMENT ON FORM PAGE NEXT TO FORMS
