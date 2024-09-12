8b51a84e9b611600601ce594e2e1d03faa08f7ed
BenjaminJMcD <BenjaminJMcD@outlook.com>
2024-09-12 11:00:11 -0500

    style: basic info document style

--- src/components/BasicDoc.jsx
+++ src/components/BasicDoc.jsx
@@ -4,17 +4,24 @@ function BasicDoc({ basicInfo }) {
 
     return (
         <div>
-            <div className="nameAndTitle">
+            <div className="basicHeader">
                 <p className="fullName">{basicInfo.firstName} {basicInfo.lastName}</p>
                 <p className="title">{basicInfo.title}</p>
+                <div className="contactInfoDoc">
+                    <p>{basicInfo.linkedIn}</p>
+                    <p>{basicInfo.gitHub}</p>
+                    <p>{basicInfo.email}</p>
+                    <p>{basicInfo.phone}</p>
+                </div>
             </div>
-            <div className="contactInfoDoc">
-                <p>{basicInfo.linkedIn}</p>
-                <p>{basicInfo.gitHub}</p>
-                <p>{basicInfo.email}</p>
-                <p>{basicInfo.phone}</p>
-            </div>
-            <p className="descriptionDoc">{basicInfo.description}</p>
+            {basicInfo.description && 
+                <div className="aboutMe">
+                    <p className="aboutMeHeader">About Me</p>
+                    <div className="descriptionDoc">
+                        <p>{basicInfo.description}</p>
+                    </div>
+                </div>
+            }
         </div>
     )
 
